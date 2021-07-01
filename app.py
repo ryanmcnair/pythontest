bf3409916230e4bb8c3de9e55cc6168f7349d1af
Ryan McNair <ryan.mcnair@gmail.com>
2021-07-01 13:43:04 -0500

    finished Python tutorial

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-weight = float(input("Weight: "))
-
-units = input("(K)g or (L)bs: ")
-
-if units.upper() == 'K':
-    converted = weight / 0.45
-    print("Weight in Lbs: " + str(converted))
-elif units.upper() == 'L':
-    converted = weight * 0.45
-    print("Weight in Kg: " + str(converted))
+numbers = (1, 2, 3)
+print(numbers.count(3))
